ea0efb8f9ee3490d96ce95855d892cc849ceffb6
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-04-06 19:10:42 +0700

    Update footer.blade.php

--- resources/views/layout/sekretaris/footer.blade.php
+++ resources/views/layout/sekretaris/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="main-footer">
+<footer class="main-footer" style="font-family: Poppins; color: #463720;" >
     <strong>Copyright &copy; Jurusan Teknologi Informasi Politeknik Negeri Malang</strong>
 </footer>
 
