22cedd853474df88637f2bdb7abc7d11ad7b2a04
hana sugianto <hanasugianto2006@gmail.com>
2024-04-06 13:22:56 +0700

    Update tampilan UMKM - Ketua RW

--- resources/views/ketua/umkm/index.blade.php
+++ resources/views/ketua/umkm/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-6">
                     <div class="form-group row">
                         <label class="col-1 control-label col-form-label">Filter:</label>
                         <div class="col-3">
@@ -25,17 +25,36 @@
                 </div>
             </div>
             <br>
-            <table class="table table-bordered table-hover table-sm" id="table_umkm">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nama Usaha</th>
-                        <th>Jenis Usaha</th>
-                        <th>Status Usaha</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-            </table>
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Pengajuan UMKM</h3>
+                    <table class="table table-bordered table-hover table-sm" id="table_ajukan_umkm">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama Usaha</th>
+                                <th>Jenis Usaha</th>
+                                <th>Status Usaha</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+                <div class="col-md-6">
+                    <h3>UMKM Terdaftar</h3>
+                    <table class="table table-bordered table-hover table-sm" id="table_umkm">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama Usaha</th>
+                                <th>Jenis Usaha</th>
+                                <th>Status Usaha</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
@@ -46,6 +65,16 @@
         #table_umkm {
             border-radius: 10px;
             /* Menambahkan radius */
+        }
+
+        h3 {
+            /* width: 497px; */
+            color: #463720;
+            font-family: Poppins;
+            font-size: 15.005px;
+            font-style: normal;
+            font-weight: 800;
+            line-height: normal;
         }
 
         #table_ajukan_umkm thead,
@@ -61,6 +90,51 @@
 @push('js')
     <script>
         $(document).ready(function() {
+            // DataTable untuk UMKM Terdaftar
+            var dataAjukanUmkm = $('#table_ajukan_umkm').DataTable({
+                serverSide: true,
+                ajax: {
+                    "url": "{{ url('ketua/umkm/list') }}",
+                    "dataType": "json",
+                    "type": "POST",
+                    "data": function(d) {
+                        d.jenis_usaha = $('#jenis_usaha').val();
+                    }
+                },
+                columns: [{
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "nama_usaha",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "jenis_usaha",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "status_usaha",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "aksi",
+                        className: "",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+
+            // DataTable untuk UMKM
             var dataUmkm = $('#table_umkm').DataTable({
                 serverSide: true,
                 ajax: {
@@ -72,37 +146,43 @@
                     }
                 },
                 columns: [{
-                    data: "DT_RowIndex",
-                    className: "text-center",
-                    orderable: false,
-                    searchable: false
-                }, {
-                    data: "nama_usaha",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                }, {
-                    data: "jenis_usaha",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                }, {
-                    data: "status_usaha",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                }, {
-                    data: "aksi",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                }]
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "nama_usaha",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "jenis_usaha",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "status_usaha",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "aksi",
+                        className: "",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
             });
 
-            S('#umkm_id').on('change', function() {
+            // Memuat ulang data ketika filter jenis usaha berubah
+            $('#jenis_usaha').on('change', function() {
                 dataUmkm.ajax.reload();
+                dataUmkmLainnya.ajax.reload();
             });
-
         });
     </script>
 @endpush
