0ec4f5ae85c199a7f4cecc768b0cca5c23b72b4e
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-04-06 10:19:25 +0700

    Create WargaModel.php

---
+++ app/Models/WargaModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class WargaModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'warga';
+    protected $primaryKey = 'warga_id';
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'kk_id', 'nik', 'nama_warga', 'tempat_tanggal_lahir', 
+        'jenis_kelamin', 'rt_rw', 'kel_desa', 'kecamatan', 
+        'agama', 'status_perkawinan', 'pekerjaan', 'hubungan_keluarga'
+    ];
+
+    public function kk(): BelongsTo
+    {
+        return $this->belongsTo(KkModel::class, 'kk_id', 'kk_id');
+    }
+}
