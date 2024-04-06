d1b77e061a2862511485af08b960478478435c7e
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-04-06 14:27:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use App\Http\Controllers\Ketua\DashboardController;
 use App\Http\Controllers\Ketua\WargaController;
+<<<<<<< Updated upstream
 use App\Http\Controllers\Ketua\UmkmController;
+=======
+use App\Http\Controllers\Sekretaris\DashboardController as SekretarisDashboardController;
+use App\Http\Controllers\Sekretaris\PengumumanController;
+use App\Http\Controllers\Sekretaris\WargaController as SekretarisWargaController;
+>>>>>>> Stashed changes
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,9 +23,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route Landing Page
 Route::get('/', [WelcomeController::class, 'index']);
+
+//Route Login Page
 Route::get('/login', [WelcomeController::class, 'login']);
 
+//Route Halaman Ketua RW
 Route::get('/ketua', [WelcomeController::class, 'ketua']);
 Route::group(['prefix' => 'ketua/dashboard'], function () {
     Route::get('/', [DashboardController::class, 'index']);
@@ -30,10 +40,25 @@ Route::group(['prefix' => 'ketua/warga'], function () {
     Route::get('/{id}', [WargaController::class, 'show']);
 });
 
+// Route Halaman Sekretaris RW
 Route::get('/sekretaris', [WelcomeController::class, 'sekretaris']);
+Route::group(['prefix' => 'sekretaris/dashboard'], function () {
+    Route::get('/', [SekretarisDashboardController::class, 'index']);
+});
+Route::group(['prefix' => 'sekretaris/warga'], function () {
+    Route::get('/', [SekretarisWargaController::class, 'index']);
+    Route::post('/list', [SekretarisWargaController::class, 'list']);
+    Route::get('/{id}', [SekretarisWargaController::class, 'show']);
+});
+Route::group(['prefix' => 'sekretaris/pengumuman'], function () {
+    Route::get('/', [PengumumanController::class, 'index']);
+    Route::get('/{id}', [PengumumanController::class, 'show']);
+});
 
+// Route Halaman Bendahara RW
 Route::get('/bendahara', [WelcomeController::class, 'bendahara']);
 
+// Route Halaman Warga
 Route::get('/warga', [WelcomeController::class, 'warga']);
 
 Route::group(['prefix' => 'ketua/umkm'], function () {
