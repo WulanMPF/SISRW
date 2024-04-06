75bf696c84d787a4da130d4234ae11b84a1c0d5d
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-04-06 11:42:12 +0700

    Update WargaController.php

--- app/Http/Controllers/ketua/WargaController.php
+++ app/Http/Controllers/ketua/WargaController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\KkModel;
 use App\Models\WargaModel;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class WargaController extends Controller
 {
@@ -15,22 +16,36 @@ class WargaController extends Controller
      */
     public function index(WargaDataTable $dataTable)
     {
-        $breadcrumb = (object) [];
+        $breadcrumb = (object) [
+            'title' => 'Data Warga RW 05',
+            'date' => date('l, d F Y'),
+            'list'  => ['Home', 'Data Warga']
+        ];
 
         $activeMenu = 'warga';
 
+        $warga = WargaModel::all();
         $kk = KkModel::all();
 
-        return view('ketua.warga.index', ['breadcrumb' => $breadcrumb, 'kk' => $kk, 'activeMenu' => $activeMenu]);
+        return view('ketua.warga.index', ['breadcrumb' => $breadcrumb, 'warga' => $warga, 'kk' => $kk, 'activeMenu' => $activeMenu]);
     }
 
     public function list(Request $request)
     {
         $warga = WargaModel::select(
-            'kk_id', 'nik', 'nama_warga', 'tempat_tanggal_lahir', 'jenis_kelamin', 
-            'rt_rw', 'kel_desa', 'kecamatan', 'agama', 'status_perkawinan', 
-            'pekerjaan', 'hubungan_keluarga'
-            )
+            'kk_id',
+            'nik',
+            'nama_warga',
+            'tempat_tgl_lahir',
+            'jenis_kelamin',
+            'rt_rw',
+            'kel_desa',
+            'kecamatan',
+            'agama',
+            'status_perkawinan',
+            'pekerjaan',
+            'hubungan_keluarga'
+        )
             ->with('kk');
 
         if ($request->kk_id) {
@@ -38,23 +53,27 @@ class WargaController extends Controller
         }
 
         return DataTables::of($warga)
-            ->addIndexColumn() 
-            ->addColumn('Anggota Keluarga', function ($warga) { 
+            ->addIndexColumn()
+            ->addColumn('Anggota Keluarga', function ($warga) {
                 $btn = '<a href="' . url('/warga/' . $warga->warga_id) . '" class="btn btn-info btn-sm">Lihat</a> ';
-                    
+
                 return $btn;
             })
-            ->rawColumns(['Anggota Keluarga']) 
+            ->rawColumns(['Anggota Keluarga'])
             ->make(true);
     }
 
     public function create()
     {
-        $breadcrumb = (object) [];
+        $breadcrumb = (object) [
+            'title' => 'Tambah Data Warga',
+            'date' => date('l, d F Y'),
+            'list'  => ['Home', 'Data Warga', 'Tambah']
+        ];
 
         $kk = KkModel::all();
 
-        $activeMenu = 'warga'; 
+        $activeMenu = 'warga';
 
         return view('ketua.warga.index', ['breadcrumb' => $breadcrumb, 'kk' => $kk, 'activeMenu' => $activeMenu]);
     }
@@ -64,8 +83,8 @@ class WargaController extends Controller
         $request->validate([
             'kk_id'                   => 'required|integer',
             'nik'                     => 'required|integer|unique:warga,nik',
-            'nama_warga'              => 'required|string|max:100', 
-            'tempat_tanggal_lahir'    => 'required|string|max:100',
+            'nama_warga'              => 'required|string|max:100',
+            'tempat_tgl_lahir'        => 'required|string|max:100',
             'jenis_kelamin'           => 'required|in:L,P',
             'rt_rw'                   => 'required|string|max:10',
             'kel_desa'                => 'required|string|max:50',
@@ -76,11 +95,11 @@ class WargaController extends Controller
             'hubungan_keluarga'       => 'required|string|max:100',
         ]);
 
-        BarangModel::create([
+        WargaModel::create([
             'kk_id'                   => $request->kk_id,
             'nik'                     => $request->nik,
             'nama_warga'              => $request->nama_warga,
-            'tempat_tanggal_lahir'    => $request->tempat_tanggal_lahir,
+            'tempat_tgl_lahir'        => $request->tempat_tanggal_lahir,
             'jenis_kelamin'           => $request->jenis_kelamin,
             'rt_rw'                   => $request->rt_rw,
             'kel_desa'                => $request->kel_desa,
@@ -98,7 +117,11 @@ class WargaController extends Controller
     {
         $warga = WargaModel::with('kk')->find($id);
 
-        $breadcrumb = (object) [];
+        $breadcrumb = (object) [
+            'title' => 'Anggota Keluarga',
+            'date' => date('l, d F Y'),
+            'list'  => ['Home', 'Data Warga', 'Anggota Keluarga']
+        ];
 
         $activeMenu = 'warga';
 
@@ -110,50 +133,65 @@ class WargaController extends Controller
         $warga = WargaModel::find($id);
         $kk = KkModel::all();
 
-        $breadcrumb = (object) [];
-
-        $page = (object) [
-            'title' => 'Edit barang'
+        $breadcrumb = (object) [
+            'title' => 'Anggota Keluarga',
+            'date' => date('l, d F Y'),
+            'list'  => ['Home', 'Data Warga', 'Anggota Keluarga', 'Edit']
         ];
 
-        $activeMenu = 'barang';
+        $activeMenu = 'warga';
 
-        return view('barang.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'barang' => $barang, 'kategori' => $kategori, 'activeMenu' => $activeMenu]);
+        return view('ketua.warga.edit', ['breadcrumb' => $breadcrumb, 'warga' => $warga, 'kk' => $kk, 'activeMenu' => $activeMenu]);
     }
 
     public function update(Request $request, string $id)
     {
         $request->validate([
-            'kategori_id'   => 'required|',
-            'barang_kode'   => 'required|string|min:3',
-            'barang_nama'   => 'required|string|max:100', 
-            'harga_beli'    => 'required|integer',
-            'harga_jual'    => 'required|integer'
+            'kk_id'                   => 'required|',
+            'nik'                     => 'required|integer|unique:warga,nik',
+            'nama_warga'              => 'required|string|max:100',
+            'tempat_tgl_lahir'        => 'required|string|max:100',
+            'jenis_kelamin'           => 'required|in:L,P',
+            'rt_rw'                   => 'required|string|max:10',
+            'kel_desa'                => 'required|string|max:50',
+            'kecamatan'               => 'required|string|max:50',
+            'agama'                   => 'required|string|max:20',
+            'status_perkawinan'       => 'required|string|max:50',
+            'pekerjaan'               => 'required|string|max:50',
+            'hubungan_keluarga'       => 'required|string|max:100',
         ]);
 
-        BarangModel::find($id)->update([
-            'kategori_id'   => $request->kategori_id,
-            'barang_kode'   => $request->barang_kode,
-            'barang_nama'   => $request->barang_nama, 
-            'harga_beli'    => $request->harga_beli,
-            'harga_jual'    => $request->harga_jual
+        WargaModel::find($id)->update([
+            'kk_id'                   => $request->kk_id,
+            'nik'                     => $request->nik,
+            'nama_warga'              => $request->nama_warga,
+            'tempat_tgl_lahir'        => $request->tempat_tanggal_lahir,
+            'jenis_kelamin'           => $request->jenis_kelamin,
+            'rt_rw'                   => $request->rt_rw,
+            'kel_desa'                => $request->kel_desa,
+            'kecamatan'               => $request->kecamatan,
+            'agama'                   => $request->agama,
+            'status_perkawinan'       => $request->status_perkawinan,
+            'pekerjaan'               => $request->pekerjaan,
+            'hubungan_keluarga'       => $request->hubungan_keluarga
         ]);
 
-        return redirect('/barang')->with('success', 'Data barang berhasil diubah');
+        return redirect('/ketua/warga')->with('success', 'Data warga berhasil diubah');
     }
 
     public function destroy(string $id)
     {
-        $check = BarangModel::find($id);
-        if (!$check) { 
-            return redirect('/barang')->with('error', 'Data barang tidak ditemukan');
+        $check = WargaModel::find($id);
+        if (!$check) {
+            return redirect('/ketua/warga')->with('error', 'Data warga tidak ditemukan');
         }
 
         try {
-            BarangModel::destroy($id); 
+            WargaModel::destroy($id);
 
-            return redirect('/barang')->with('success', 'Data barang berhasil dihapus');
+            return redirect('/ketua/warga')->with('success', 'Data warga berhasil dihapus');
         } catch (\Illuminate\Database\QueryException $e) {
-            return redirect('/barang')->with('error', 'Data barang gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+            return redirect('/ketua/warga')->with('error', 'Data warga gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
+    }
 }
