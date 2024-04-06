aed77ff946600dd9745e12eedc20da7b7fc7a24e
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-04-06 19:10:49 +0700

    Update header.blade.php

--- resources/views/layout/sekretaris/header.blade.php
+++ resources/views/layout/sekretaris/header.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light" style="font-family: Poppins; color: #463720;" >
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
