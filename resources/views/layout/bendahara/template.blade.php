a6ce1bbe2b78dc16af3c8cbe248e476015510e38
hana sugianto <hanasugianto2006@gmail.com>
2024-04-06 18:36:30 +0700

    Update template -- layout bendahara

--- resources/views/layout/bendahara/template.blade.php
+++ resources/views/layout/bendahara/template.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('Ketua RW', 'Ketua RW') }}</title>
+    <title>{{ config('Bendahara RW', 'Bendahara RW') }}</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Untuk mengirimkan token Laravel CSRF pada setiap request ajax -->
