41548d15496fd3d96334a4a9a62ab3b34704dc70
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-05-01 11:05:48 +0700

    Create ProfileController.php

---
+++ app/Http/Controllers/Bendahara/ProfileController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Bendahara;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Profile',
+            'date' => date('l, d F Y'),
+            'list'  => ['Home', 'Profile']
+        ];
+
+        $activeMenu = 'profile';
+
+        return view('bendahara.profile.index', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+    }
+}
