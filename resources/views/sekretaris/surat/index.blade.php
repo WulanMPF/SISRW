fed137d0f779c8d8247bba9190c2ec745449fd6c
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-05-14 20:50:14 +0700

    Update index.blade.php

--- resources/views/sekretaris/surat/index.blade.php
+++ resources/views/sekretaris/surat/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.ketua.template')
+@extends('layout.sekretaris.template')
 
 @section('content')
     <div class="card card-outline card-light">
@@ -9,53 +9,36 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-            <div class="row justify-content-between"> <!-- Tambahkan kelas justify-content-between di sini -->
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
-                            <select class="form-control" id="status_warga" name="status_warga" required>
-                                <option value="Tampilkan"></option>
-                                <option>Tinggal Tetap</option>
-                                <option>Tinggal Sementara</option>
-                            </select>
-                            <small class="form-text text-muted">Status Kependudukan</small>
-                        </div>
-                        {{-- <div class="col-md-4 text-right">
-                            <a class="btn btn-sm mt-1 btn-tambah" href="{{ url('sekretaris/surat/create') }}">+ Buat Surat</a>
-                        </div> --}}
-                    </div>
-                </div>
+            <div class="col-md text-right">
+                <a class="btn btn-sm mt-1 btn-tambah" href="{{ url('/sekretaris/surat/create') }}">+ Buat Surat</a>
             </div>
             <br>
             <div class="row">
                 <div class="col-md-6">
                     <h3>Surat Masuk</h3>
-                    {{-- <table class="table table-bordered table-hover table-sm" id="table_ajukan_umkm" style="text-align: center;">
+                    <table class="table table-bordered table-hover table-sm" id="table_surat_masuk"
+                        style="text-align: center;">
                         <thead>
                             <tr>
-                                <th>ID</th>
-                                <th>Nama Usaha</th>
-                                <th>Jenis Usaha</th>
-                                <th>Status Usaha</th>
-                                <th>Aksi</th>
+                                <th>No</th>
+                                <th>Nama Surat</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
-                    </table> --}}
+                    </table>
                 </div>
                 <div class="col-md-6">
                     <h3>Surat Keluar</h3>
-                    {{-- <table class="table table-bordered table-hover table-sm" id="table_umkm" style="text-align: center;">
+                    <table class="table table-bordered table-hover table-sm" id="table_surat_keluar"
+                        style="text-align: center;">
                         <thead>
                             <tr>
-                                <th>ID</th>
-                                <th>Nama Usaha</th>
-                                <th>Jenis Usaha</th>
-                                <th>Status Usaha</th>
-                                <th>Aksi</th>
+                                <th>No</th>
+                                <th>Nama Surat</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
-                    </table> --}}
+                    </table>
                 </div>
             </div>
         </div>
@@ -64,11 +47,11 @@
 
 @push('css')
     <style>
-        /* #table_ajukan_umkm,
-        #table_umkm {
+        #table_surat_masuk,
+        #table_surat_keluar {
             border-radius: 10px;
             overflow: hidden;
-        } */
+        }
 
         h3 {
             /* width: 497px; */
@@ -80,12 +63,13 @@
             line-height: normal;
         }
 
-        /* #table_ajukan_umkm thead,
-        #table_umkm thead {
+        #table_surat_masuk thead,
+        #table_surat_keluar thead {
             background-color: #d9d2c7;
             /* Warna latar belakang coklat */
             color: #7F643C;
-        } */
+            /* Warna teks putih */
+        }
 
         .btn-tambah {
             background-color: #BB955C;
@@ -96,17 +80,17 @@
 @endpush
 
 @push('js')
-    {{-- <script>
+    <script>
         $(document).ready(function() {
-            // DataTable untuk UMKM Terdaftar
-            var dataAjukanUmkm = $('#table_ajukan_umkm').DataTable({
+            // DataTable untuk Surat Masuk
+            var dataSuratMasuk = $('#table_surat_masuk').DataTable({
                 serverSide: true,
                 ajax: {
-                    "url": "{{ url('ketua/umkm/list') }}",
-                    "dataType": "json",
-                    "type": "POST",
-                    "data": function(d) {
-                        d.jenis_usaha = $('#jenis_usaha').val();
+                    url: "{{ url('sekretaris/surat/list') }}",
+                    dataType: "json",
+                    type: "POST",
+                    data: function(d) {
+                        d.jenis_surat = 'Masuk';
                     }
                 },
                 columns: [{
@@ -116,25 +100,13 @@
                         searchable: false
                     },
                     {
-                        data: "nama_usaha",
+                        data: "nama_surat",
                         className: "",
                         orderable: true,
                         searchable: true
                     },
                     {
-                        data: "jenis_usaha",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "status_usaha",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "aksi",
+                        data: "Action",
                         className: "",
                         orderable: false,
                         searchable: false
@@ -142,15 +114,15 @@
                 ]
             });
 
-            // DataTable untuk UMKM
-            var dataUmkm = $('#table_umkm').DataTable({
+            // DataTable untuk Surat Keluar
+            var dataSuratKeluar = $('#table_surat_keluar').DataTable({
                 serverSide: true,
                 ajax: {
-                    "url": "{{ url('ketua/umkm/list') }}",
-                    "dataType": "json",
-                    "type": "POST",
-                    "data": function(d) {
-                        d.jenis_usaha = $('#jenis_usaha').val();
+                    url: "{{ url('sekretaris/surat/list') }}",
+                    dataType: "json",
+                    type: "POST",
+                    data: function(d) {
+                        d.jenis_surat = 'Keluar';
                     }
                 },
                 columns: [{
@@ -160,37 +132,19 @@
                         searchable: false
                     },
                     {
-                        data: "nama_usaha",
+                        data: "nama_surat",
                         className: "",
                         orderable: true,
                         searchable: true
                     },
                     {
-                        data: "jenis_usaha",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "status_usaha",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "aksi",
+                        data: "Action",
                         className: "",
                         orderable: false,
                         searchable: false
                     }
                 ]
-            });
-
-            // Memuat ulang data ketika filter jenis usaha berubah
-            $('#jenis_usaha').on('change', function() {
-                dataUmkm.ajax.reload();
-                dataUmkmLainnya.ajax.reload();
             });
         });
-    </script> --}}
+    </script>
 @endpush
