877c04f1ea93f3d50b869d0f42086791a2db3fb7
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2024-05-15 05:21:50 +0700

    Update Laporan Keuangan Seeder

--- database/seeders/LapKeuSeeder.php
+++ database/seeders/LapKeuSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class LapKeuSeeder extends Seeder
 {
@@ -19,35 +19,45 @@ class LapKeuSeeder extends Seeder
                 'nominal' => 10000000,
                 'keterangan' => 'Iuran Bulanan Bulan Maret',
                 'jenis_laporan' => 'pemasukan',
-                'tgl_laporan' => '2024-04-01'
+                'tgl_laporan' => '2024-04-01',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'laporan_id' => 2,
                 'nominal' => 200000,
                 'keterangan' => 'Pembelian bahan FC Surat',
                 'jenis_laporan' => 'pengeluaran',
-                'tgl_laporan' => '2024-04-03'
+                'tgl_laporan' => '2024-04-03',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'laporan_id' => 3,
                 'nominal' => 10000000,
                 'keterangan' => 'Iuran Bulanan Bulan Mei',
                 'jenis_laporan' => 'pemasukan',
-                'tgl_laporan' => '2024-05-01'
+                'tgl_laporan' => '2024-05-01',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'laporan_id' => 4,
                 'nominal' => 100000,
                 'keterangan' => 'Acara Kerja Bakti',
                 'jenis_laporan' => 'pengeluaran',
-                'tgl_laporan' => '2024-05-07'
+                'tgl_laporan' => '2024-05-07',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'laporan_id' => 5,
                 'nominal' => 150000,
                 'keterangan' => 'Iuran Sampah',
                 'jenis_laporan' => 'pemasukan',
-                'tgl_laporan' => '2024-05-09'
+                'tgl_laporan' => '2024-05-09',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
         ]);
     }
