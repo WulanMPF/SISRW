2e405ad6013d08e5df2b0f7a5161c3fa269f618e
cindy vira safitri <cindyvira929@gmail.com>
2024-05-14 10:46:46 +0700

    Update LapkeuController.php

--- app/Http/Controllers/ketua/LapkeuController.php
+++ app/Http/Controllers/ketua/LapkeuController.php
@@ -30,15 +30,20 @@ class LapkeuController extends Controller
     {
         $laporans = LapkeuModel::select('laporan_id', 'nominal', 'keterangan', 'jenis_laporan', 'tgl_laporan');
 
-        if ($request->jenis_laporan) {
+        if ($request->has('jenis_laporan') && $request->jenis_laporan != '') {
             $laporans->where('jenis_laporan', $request->jenis_laporan);
         }
 
         return DataTables::of($laporans)
-            ->addIndexColumn() // Menambahkan kolom index / no urut (default nmaa kolom: DT_RowINdex)
+            ->addIndexColumn() // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+            ->addColumn('pemasukan', function ($laporan) {
+                return $laporan->jenis_laporan == 'pemasukan' ? $laporan->nominal : 0;
+            })
+            ->addColumn('pengeluaran', function ($laporan) {
+                return $laporan->jenis_laporan == 'pengeluaran' ? $laporan->nominal : 0;
+            })
             ->addColumn('aksi', function ($laporan) {
-                $btn = '<a href="' . url('/laporan/' . $laporan->laporan_id) . '" class="btn btn-info btn-sm">Lihat Detail</a>  &nbsp;';
-
+                $btn = '<a href="' . url('/laporan/' . $laporan->laporan_id) . '" class="btn btn-info btn-sm">Lihat Detail</a>';
                 return $btn;
             })
             ->rawColumns(['aksi'])
