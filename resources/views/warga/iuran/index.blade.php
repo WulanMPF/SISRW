017195e29940d25fa6f4ef59820fc0243d33d7b0
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2024-05-15 09:06:28 +0700

    Update index.blade.php

--- resources/views/warga/iuran/index.blade.php
+++ resources/views/warga/iuran/index.blade.php
@@ -3,17 +3,20 @@
 @section('content')
     <div class="card card-outline card-light">
         <div class="card-body">
+            <!-- Displaying Success and Error Messages -->
             @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
             @endif
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
+            
+            <!-- Filter Section -->
             <div class="row mb-3">
                 <div class="col-md-12">
                     <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
+                        <label class="col-sm-1 control-label col-form-label">Filter:</label>
+                        <div class="col-sm-3">
                             <select class="form-control" id="status_pembayaran" name="status_pembayaran" required>
                                 <option value="">Tampilkan Semua</option>
                                 <option value="Lunas">Lunas</option>
@@ -22,37 +25,40 @@
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <!-- Information Boxes -->
+            <div class="d-flex">
                 <div class="small-box bg-light">
                     <div class="inner">
                         <h4>Iuran Bulanan</h4>
-
                         <p><span style="color: green;">Rp. 10.000,-</span></p>
                     </div>
                 </div>
                 <div class="small-box bg-light">
                     <div class="inner">
                         <h4>Iuran Sampah</h4>
-
                         <p><span style="color: green;">Rp. 10.000,-</span></p>
                     </div>
                 </div>
                 <div class="small-box bg-light">
                     <div class="inner">
                         <h4>Iuran Keamanan</h4>
-
                         <p><span style="color: green;">Rp. 10.000,-</span></p>
                     </div>
                 </div>
             </div>
+
+            <!-- Table -->
             <table class="table table-bordered table-striped table-hover table-sm" id="table_iuran">
                 <thead>
                     <tr>
                         <th>No</th>
                         <th>Periode Pembayaran</th>
-                        <th>No. KK </th>
+                        <th>No. KK</th>
+                        <th>Iuran Bukanan</th>
                         <th>Iuran Sampah</th>
                         <th>Iuran Keamanan</th>
-                        <th>Iuran Bulanan</th>
                     </tr>
                 </thead>
             </table>
@@ -61,60 +67,51 @@
 @endsection
 
 @push('css')
-    <style>
-        #table_iuran {
-            border-radius: 10px;
-            overflow: hidden;
-        }
-
-        #table_iuran thead {
-            background-color: #d9d2c7;
-            /* Warna latar belakang coklat */
-            color: #7F643C;
-            /* Warna teks putih */
-        }
-       
+<style>
     .small-box {
-        margin-right: 50px; /* Menambahkan margin kanan sebesar 10 piksel */
+        margin-right: 50px; /* Adjusted margin for alignment */
         color: #463720;
-        font-family: Poppins;
-        font-size: 23.95px;
-        font-style: normal;
+        font-family: 'Poppins', sans-serif;
+        font-size: 25px;
         font-weight: 700;
-        line-height: normal;
     }
 
-    </style>
+    #table_iuran {
+        border-radius: 10px;
+    }
+
+    #table_iuran thead {
+        background-color: #d9d2c7;
+        color: #7F643C;
+    }
+</style>
 @endpush
 
 @push('js')
-    <script>
-        $(document).ready(function() {
-            var dataIuran = $('#table_iuran').DataTable({
-                serverSide: true,
-                ajax: {
-                    "url": "{{ url('warga/iuran/list') }}",
-                    "dataType": "json",
-                    "type": "POST",
-                    "data": function(d) {
-                        d._status_pembayaran = $('#status_pembayaran').val();
-                    }
-                },
-                columns: [
-                    { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
-                    { data: "tgl_pembayaran", className: "", orderable: true, searchable: true },
-                    { data: "kk.no_kk", className: "", orderable: true, searchable: true },
-                    { data: "jenis_iuran", className: "", orderable: true, searchable: true },
-                    { data: "status_pembayaran", className: "", orderable: true, searchable: false }
-                    
-                   
-                ]
-            });
-
-            $('#status_pembayaran').on('change', function() {
-                dataIuran.ajax.reload();
-            });
-
+<script>
+    $(document).ready(function() {
+        var dataIuran = $('#table_iuran').DataTable({
+            serverSide: true,
+            ajax: {
+                url: "{{ url('warga/iuran/list') }}",
+                type: "POST",
+                dataType: "json",
+                data: function (d) {
+                    d._status_pembayaran = $('#status_pembayaran').val();
+                }
+            },
+            columns: [
+                { data: 'DT_RowIndex', className: 'text-center', orderable: false, searchable: false },
+                { data: 'tgl_pembayaran', orderable: true, searchable: true },
+                { data: 'kk.no_kk', orderable: true, searchable: true },
+                { data: 'jenis_iuran', orderable: true, searchable: true },
+                { data: 'status_pembayaran', orderable: true, searchable: false }
+            ]
         });
-    </script>
+
+        $('#status_pembayaran').on('change', function () {
+            dataIuran.ajax.reload();
+        });
+    });
+</script>
 @endpush
