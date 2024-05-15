15e95941a89956c24bb97b23df0e71734ba7f4ff
cindy vira safitri <cindyvira929@gmail.com>
2024-05-15 10:02:53 +0700

    Update PengaduanSeeder.php

--- database/seeders/PengaduanSeeder.php
+++ database/seeders/PengaduanSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\PengaduanModel;
 use App\Models\WargaModel;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class PengaduanSeeder extends Seeder
 {
@@ -16,43 +17,62 @@ class PengaduanSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
-
-        // Ambil semua ID dari model WargaModel
-        $wargaIds = WargaModel::pluck('warga_id')->toArray();
-
-        $data = [];
-
-        // Generate dummy data untuk PengaduanModel
-        for ($i = 0; $i < 10; $i++) {
-            $wargaId = $faker->randomElement($wargaIds);
-            $namaPelapor = $faker->name;
-            $jenisPengaduan = $faker->randomElement(['Kerusakan Jalan', 'Kebisingan', 'Pencurian']);
-            $tanggalPengaduan = $faker->dateTimeBetween('-30 days', 'now');
-            $prioritas = $faker->randomElement(['Tinggi', 'Sedang', 'Rendah']);
-            $statusPengaduan = $faker->randomElement(['Ditunda', 'Diproses', 'Selesai']);
-            $deskripsi = $faker->sentence;
-            $lampiran = $faker->fileExtension;
-            $tindakanDiambil = $faker->sentence;
-            $createdAt = $faker->dateTimeBetween('-1 year', 'now');
-            $updatedAt = $faker->dateTimeBetween($createdAt, 'now');
-
-            $data[] = [
-                'warga_id' => $wargaId,
-                'nama_pelapor' => $namaPelapor,
-                'jenis_pengaduan' => $jenisPengaduan,
-                'tgl_pengaduan' => $tanggalPengaduan,
-                'prioritas' => $prioritas,
-                'status_pengaduan' => $statusPengaduan,
-                'deskripsi' => $deskripsi,
-                'lampiran' => $lampiran,
-                'tindakan_diambil' => $tindakanDiambil,
-                'created_at' => $createdAt,
-                'updated_at' => $updatedAt,
-            ];
-        }
-
-        // Insert data ke dalam tabel pengaduan menggunakan model PengaduanModel
-        PengaduanModel::insert($data);
+        DB::table('pengaduan')->insert([
+            [
+                'pengaduan_id' => 1,
+                'warga_id' => 1,
+                'jenis_pengaduan' => 'Kebersihan',
+                'tgl_pengaduan' => '2024-04-01',
+                'prioritas' => 'Tinggi',
+                'status_pengaduan' => 'Ditunda',
+                'deskripsi' => 'Sampah menumpuk di area RT 01.',
+                'lampiran' => 'sampah_rt01.jpg',
+                'tindakan_diambil' => 'Belum ada tindakan.'
+            ],
+            [
+                'pengaduan_id' => 2,
+                'warga_id' => 2,
+                'jenis_pengaduan' => 'Keamanan',
+                'tgl_pengaduan' => '2024-04-05',
+                'prioritas' => 'Sedang',
+                'status_pengaduan' => 'Diproses',
+                'deskripsi' => 'Ada orang asing mencurigakan di sekitar RT 03.',
+                'lampiran' => 'orang_asing_rt03.jpg',
+                'tindakan_diambil' => 'Patroli keamanan dilakukan.'
+            ],
+            [
+                'pengaduan_id' => 3,
+                'warga_id' => 3,
+                'jenis_pengaduan' => 'Infrastruktur',
+                'tgl_pengaduan' => '2024-04-10',
+                'prioritas' => 'Rendah',
+                'status_pengaduan' => 'Selesai',
+                'deskripsi' => 'Jalan rusak di depan rumah nomor 12.',
+                'lampiran' => 'jalan_rusak.jpg',
+                'tindakan_diambil' => 'Jalan sudah diperbaiki.'
+            ],
+            [
+                'pengaduan_id' => 4,
+                'warga_id' => 4,
+                'jenis_pengaduan' => 'Kesehatan',
+                'tgl_pengaduan' => '2024-04-16',
+                'prioritas' => 'Tinggi',
+                'status_pengaduan' => 'Diproses',
+                'deskripsi' => 'Banyak warga terkena demam berdarah.',
+                'lampiran' => 'dbd_warga.jpg',
+                'tindakan_diambil' => 'Fogging akan dilakukan.'
+            ],
+            [
+                'pengaduan_id' => 5,
+                'warga_id' => 5,
+                'jenis_pengaduan' => 'Pelayanan',
+                'tgl_pengaduan' => '2024-04-20',
+                'prioritas' => 'Sedang',
+                'status_pengaduan' => 'Ditunda',
+                'deskripsi' => 'Pelayanan administrasi lambat.',
+                'lampiran' => 'administrasi_lambat.jpg',
+                'tindakan_diambil' => 'Akan ditindaklanjuti.'
+            ],
+        ]);
     }
 }
