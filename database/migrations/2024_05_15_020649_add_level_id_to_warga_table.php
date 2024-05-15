c0cf59f5bcd58d0b43a6dbeea5ebd650c8ba0a06
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-05-15 10:38:54 +0700

    Create 2024_05_15_020649_add_level_id_to_warga_table.php

---
+++ database/migrations/2024_05_15_020649_add_level_id_to_warga_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('warga', function (Blueprint $table) {
+
+            // Tambahkan kolom foreign key warga_id
+            $table->unsignedBigInteger('level_id')->index();
+            // Definisikan constraint foreign key
+            $table->foreign('level_id')->references('level_id')->on('level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('warga', function (Blueprint $table) {
+            // Hapus foreign key
+            $table->dropForeign(['level_id']);
+
+            // Hapus kolom foreign key
+            $table->dropColumn('level_id');
+        });
+    }
+};
