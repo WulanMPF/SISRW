3b594eed2c560ee655842306f48372b5ff5ee659
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-05-15 10:38:35 +0700

    Update UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,13 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Model
+class UserModel extends \Illuminate\Foundation\Auth\User
 {
+    
     use HasFactory;
 
     protected $table = 'user';
     protected $primaryKey = 'user_id';
+    protected $fillable = ['level_id', 'warga_id', 'username', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(WargaModel::class, 'level_id', 'level_id');
+    }
 
     public function warga(): BelongsTo
     {
