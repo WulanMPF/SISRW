0062608f3df32661e43e4289281b5247ff3b07f1
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2024-05-28 15:59:30 +0700

    update iuran

--- resources/views/bendahara/iuran/create.blade.php
+++ resources/views/bendahara/iuran/create.blade.php
@@ -1,49 +1,45 @@
-@extends('layout.bendahara.template')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h2>{{ $breadcrumb->title }}</h2>
-            <p>{{ $breadcrumb->date }}</p>
-            <ol class="breadcrumb">
-                @foreach($breadcrumb->list as $item)
-                    <li class="breadcrumb-item">{{ $item }}</li>
-                @endforeach
-                <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
-            </ol>
-
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
+            <h2>Tambah Data Iuran Baru</h2>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
             @endif
 
             <form action="{{ route('iuran.store') }}" method="POST">
                 @csrf
                 <div class="form-group">
-                    <label for="kk_id">KK</label>
-                    <select name="kk_id" id="kk_id" class="form-control" required>
-                        @foreach($kk as $k)
-                            <option value="{{ $k->id }}">{{ $k->nama }}</option>
+                    <label for="kk_id">KK ID</label>
+                    <select class="form-control" id="kk_id" name="kk_id">
+                        @foreach ($kk as $k)
+                            <option value="{{ $k->id }}">{{ $k->id }}</option>
                         @endforeach
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="tgl_pembayaran">Tanggal Pembayaran</label>
-                    <input type="date" name="tgl_pembayaran" id="tgl_pembayaran" class="form-control" required>
+                    <input type="date" class="form-control" id="tgl_pembayaran" name="tgl_pembayaran">
                 </div>
                 <div class="form-group">
                     <label for="jenis_iuran">Jenis Iuran</label>
-                    <input type="text" name="jenis_iuran" id="jenis_iuran" class="form-control" required>
+                    <input type="text" class="form-control" id="jenis_iuran" name="jenis_iuran">
                 </div>
                 <div class="form-group">
                     <label for="jumlah_bayar">Jumlah Bayar</label>
-                    <input type="number" name="jumlah_bayar" id="jumlah_bayar" class="form-control" required>
+                    <input type="text" class="form-control" id="jumlah_bayar" name="jumlah_bayar">
                 </div>
                 <div class="form-group">
                     <label for="status_pembayaran">Status Pembayaran</label>
-                    <input type="text" name="status_pembayaran" id="status_pembayaran" class="form-control" required>
+                    <input type="text" class="form-control" id="status_pembayaran" name="status_pembayaran">
                 </div>
                 <button type="submit" class="btn btn-primary">Simpan</button>
             </form>
