5bef0f69e0de9f5b037a627e157cfe64ddebff4b
cindy vira safitri <cindyvira929@gmail.com>
2024-05-30 16:07:25 +0700

    Update PengaduanController.php

--- app/Http/Controllers/ketua/PengaduanController.php
+++ app/Http/Controllers/ketua/PengaduanController.php
@@ -36,14 +36,42 @@ class PengaduanController extends Controller
                 'warga.nama_warga as nama_pelapor',
             ]);
 
+        if ($request->status_pengaduan) {
+            $pengaduan->where('status_pengaduan', $request->status_pengaduan);
+        }
+
+        $pengaduan->orderBy('tgl_pengaduan', 'desc');
+
         return DataTables::of($pengaduan)
             ->addIndexColumn()
-            ->addColumn('action', function ($row) {
-                return '<a href="' . route('pengaduan.show', $row->pengaduan_id) . '" class="btn btn-sm" style="background-color: #BB955C; color: white; border-radius: 9px;">Detail</a>';
+            ->addColumn('prioritas', function ($row) {
+                $color = '';
+                if ($row->prioritas == 'Tinggi') {
+                    $color = 'text-danger'; // Merah
+                } elseif ($row->prioritas == 'Sedang') {
+                    $color = 'text-yellow-brown'; // Kuning sedikit coklat
+                } elseif ($row->prioritas == 'Rendah') {
+                    $color = 'text-success'; // Hijau
+                }
+                return '<span class="' . $color . '">' . $row->prioritas . '</span>';
             })
-            ->rawColumns(['action'])
+            ->addColumn('action', function ($row) {
+                $showUrl = route('pengaduan.show', $row->pengaduan_id);
+                $editUrl = url('/ketua/pengaduan/' . $row->pengaduan_id . '/edit');
+
+                $btn = '<a href="' . $showUrl . '" class="btn btn-xs btn-primary" style="border-radius: 6px;"><i class="fas fa-info-circle fa-lg"></i></a>';
+
+                if ($row->status_pengaduan == 'Diproses' || $row->status_pengaduan == 'Ditunda') {
+                    $btn = '<a href="' . $editUrl . '" class="btn btn-xs btn-warning mr-2" style="border-radius: 6px;"><i class="fas fa-edit fa-lg"></i></a>' . $btn;
+                }
+
+                return $btn;
+            })
+            ->rawColumns(['prioritas', 'action'])
             ->make(true);
     }
+
+
     public function show($id)
     {
         $pengaduan = PengaduanModel::findOrFail($id);
@@ -57,5 +85,45 @@ class PengaduanController extends Controller
         $activeMenu = 'pengaduan';
 
         return view('ketua.pengaduan.show', compact('breadcrumb', 'activeMenu', 'pengaduan'));
+    }
+
+    public function edit(string $id)
+    {
+        $pengaduan = PengaduanModel::find($id);
+
+        $breadcrumb = (object)[
+            'title' => 'Edit Pengaduan',
+            'date' => date('l, d F Y'),
+            'list' => ['Home', 'Pengaduan', 'Edit']
+        ];
+
+        $page = (object)[
+            'title' => 'Edit Pengaduan'
+        ];
+
+        $activeMenu = 'pengaduan';
+
+        return view('ketua.pengaduan.edit', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'pengaduan' => $pengaduan,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'tindakan_diambil' => 'required|string|max:200',
+            'status_pengaduan' => 'required|string|in:Diproses,Ditunda,Selesai'
+        ]);
+
+        $pengaduan = PengaduanModel::find($id);
+        $pengaduan->update([
+            'tindakan_diambil' => $request->tindakan_diambil,
+            'status_pengaduan' => $request->status_pengaduan,
+        ]);
+
+        return redirect()->route('pengaduan.index')->with('success', 'Data pengaduan berhasil diupdate');
     }
 }
