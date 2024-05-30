c4c9b836ecbd83656ca6cbc642ae4a8b47ff8fa6
cindy vira safitri <cindyvira929@gmail.com>
2024-05-30 16:07:33 +0700

    Update index.blade.php

--- resources/views/ketua/pengaduan/index.blade.php
+++ resources/views/ketua/pengaduan/index.blade.php
@@ -11,24 +11,25 @@
             @endif
             <div class="row">
                 <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
-                            <select class="form-control" id="status_pengaduan" name="status_pengaduan" required>
-                                <option value="">Tampilkan Semua</option>
-                                <option value="Tinggi">Tinggi</option>
-                                <option value="Sedang">Sedang</option>
-                                <option value="Rendah">Rendah</option>
-                            </select>
-                            <small class="form-text text-muted">Status Prioritas</small>
+                    <h3>Pengaduan Warga RW 05</h3>
+                    <div class="col-sm-12 col-md-5">
+                        <div class="form-group row">
+                            <div class="col-4">
+                                <select name="status_pengaduan" id="status_pengaduan" class="form-control rounded-select" required>
+                                    {{-- <option value="">- Semua -</option> --}}
+                                    <option value="Selesai">- Selesai -</option>
+                                    <option value="Diproses">- Diproses -</option>
+                                    <option value="Ditunda">- Ditunda -</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_pengaduan" style="text-align: center;">
+            <table class="table table-bordered table-hover table-sm" id="table_pengaduan" style="text-align: center;">
                 <thead>
                     <tr>
-                        <th>No</th>
+                        {{-- <th>No</th> --}}
                         <th>Tanggal Pengaduan</th>
                         <th>Nama Pelapor</th>
                         <th>Jenis Pengaduan</th>
@@ -51,73 +52,53 @@
 
         #table_pengaduan thead {
             background-color: #d9d2c7;
-            /* Warna latar belakang coklat */
             color: #7F643C;
-            /* Warna teks putih */
+        }
+
+        .text-danger {
+            color: red !important;
+        }
+
+        .text-warning {
+            color: yellow !important;
+        }
+
+        .text-success {
+            color: green !important;
+        }
+
+        .text-yellow-brown {
+            color: #b58900 !important; /* Kuning sedikit coklat */
         }
     </style>
 @endpush
 
 @push('js')
     <script>
-        var dataPengaduan = $('#table_pengaduan').DataTable({
-    serverSide: true,
-    ajax: {
-        "url": "{{ url('ketua/pengaduan/list') }}",
-        "dataType": "json",
-        "type": "POST",
-        "data": function(d) {
-            d._status_pengaduan = $('#status_pengaduan').val();
-        }
-    },
-                columns: [{
-                        data: "DT_RowIndex",
-                        className: "text-center",
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: "tgl_pengaduan",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "nama_pelapor",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "jenis_pengaduan",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "prioritas",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "status_pengaduan",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "action",
-                        className: "text-center",
-                        orderable: false,
-                        searchable: false
+        $(document).ready(function() {
+            var dataPengaduan = $('#table_pengaduan').DataTable({
+                serverSide: true,
+                ajax: {
+                    url: "{{ url('ketua/pengaduan/list') }}",
+                    dataType: "json",
+                    type: "POST",
+                    data: function(d) {
+                        d.status_pengaduan = $('#status_pengaduan').val();
                     }
+                },
+                columns: [
+                    { data: "tgl_pengaduan", orderable: true, searchable: true },
+                    { data: "nama_pelapor", orderable: true, searchable: true },
+                    { data: "jenis_pengaduan", orderable: true, searchable: true },
+                    { data: "prioritas", orderable: true, searchable: true },
+                    { data: "status_pengaduan", orderable: true, searchable: true },
+                    { data: "action", className: "text-center", orderable: false, searchable: false }
                 ]
             });
 
             $('#status_pengaduan').on('change', function() {
                 dataPengaduan.ajax.reload();
             });
-
+        });
     </script>
 @endpush
