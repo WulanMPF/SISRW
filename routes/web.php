9669359130f4330c124a18ec7ef29bc981990094
cindy vira safitri <cindyvira929@gmail.com>
2024-05-30 16:07:37 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -147,10 +147,12 @@ Route::group(['prefix' => 'ketua/laporan'], function () {
     Route::get('/{id}', [LapkeuController::class, 'show']);
 });
 Route::group(['prefix' => 'ketua/pengaduan'], function () {
-    Route::get('/', [PengaduanController::class, 'index']);
+    Route::get('/', [PengaduanController::class, 'index'])->name('pengaduan.index');
     Route::post('/list', [PengaduanController::class, 'list']);
     Route::get('/{id}', [PengaduanController::class, 'show'])->name('pengaduan.show');
     Route::get('/{id}/show', [PengaduanController::class, 'show'])->name('pengaduan.show');
+    Route::get('/{id}/edit', [PengaduanController::class, 'edit']);
+    Route::put('/{id}', [PengaduanController::class, 'update'])->name('pengaduan.update');
 });
 Route::group(['prefix' => 'ketua/iuran'], function () {
     Route::get('/', [IuranController::class, 'index']);
