703130d7e8cab81cd7588c59a16526a41f280e61
cindy vira safitri <cindyvira929@gmail.com>
2024-06-01 15:32:13 +0700

    Update ProfileController.php

--- app/Http/Controllers/Warga/ProfileController.php
+++ app/Http/Controllers/Warga/ProfileController.php
@@ -4,19 +4,35 @@ namespace App\Http\Controllers\Warga;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\WargaModel;
 
 class ProfileController extends Controller
 {
     public function index()
     {
+        // Ambil data pengguna yang sedang login
+        $user = Auth::user();
+
+        // Ambil data warga berdasarkan warga_id dari user yang sedang login
+        $warga = WargaModel::where('warga_id', $user->warga_id)->first();
+
+        // Buat objek breadcrumb
         $breadcrumb = (object) [
             'title' => 'Profile',
             'date' => date('l, d F Y'),
             'list'  => ['Home', 'Profile']
         ];
 
+        // Set menu aktif
         $activeMenu = 'profile';
 
-        return view('warga.profile.index', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        // Pass data pengguna dan data warga ke view
+        return view('warga.profile.index', [
+            'user' => $user,
+            'warga' => $warga,
+            'breadcrumb' => $breadcrumb,
+            'activeMenu' => $activeMenu
+        ]);
     }
 }
