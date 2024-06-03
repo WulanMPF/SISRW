9f42a866e2aa2ad622d94f3969fffc171d9bd5a8
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2024-06-04 00:36:38 +0700

    Fixed Sidebar

--- resources/views/layout/warga/header.blade.php
+++ resources/views/layout/warga/header.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light" style="font-family: Poppins; color: #463720;">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light" style="font-family: Poppins; color: #463720; ">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
@@ -31,3 +31,14 @@
     </ul>
 </nav>
 <!-- /.navbar -->
+
+<style>
+.elemen {
+    font-family: Poppins;
+    color: #463720;
+    position: fixed;
+
+    }
+
+</style>
+
--- resources/views/layout/warga/sidebar.blade.php
+++ resources/views/layout/warga/sidebar.blade.php
@@ -76,6 +76,15 @@
         background: white;
         box-shadow: 0 14px 28px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .22) !important;
     }
+
+    body:not(.layout-fixed) .main-sidebar {
+        height: inherit;
+        min-height: 100%;
+        position: fixed;
+        top: 0;
+        }
+
+    
 </style>
 
 <script>
